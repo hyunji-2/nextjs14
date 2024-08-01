86e756ddce296f753dce4fd684d09bf99ac65ccc
hyunji-2 <aaa9013@gmail.com>
2024-08-01 11:24:31 +0900

    ~ #4.4

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -3,12 +3,13 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
+import styles from "../styles/navigation.module.css";
 
 export default function Navigation() {
   const path = usePathname();
   const [count, setCount] = useState(0);
   return (
-    <nav>
+    <nav className={styles.nav}>
       <ul>
         <li>
           <Link href="/">Home</Link> {path === "/" ? "👍" : ""}
@@ -17,9 +18,9 @@ export default function Navigation() {
           <Link href="/about-us">About Us</Link>{" "}
           {path === "/about-us" ? "👍" : ""}
         </li>
-        <li>
+        {/* <li>
           <button onClick={() => setCount((c) => c + 1)}>{count}</button>
-        </li>
+        </li> */}
       </ul>
     </nav>
   );
